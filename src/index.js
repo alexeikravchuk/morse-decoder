c26e71b1e3977227192d0a443f102b8d0a6a36b3
Alexei Kravchuk <alexei.kravchuk21@gmail.com>
2020-03-04 22:38:41 +0300

    feat: task "morse decoder" done

--- src/index.js
+++ src/index.js
@@ -38,7 +38,27 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let lettersArray = [];
+    for(let i = 0; i < expr.length / 10; i++) {
+        numbers = expr.slice(i * 10, i * 10 + 10);
+        lettersArray[i] = '';
+        for(let j = 0; j < 5; j++) {
+          switch (numbers.slice(j * 2, j * 2 + 2)) {
+            case "00" : break;
+            case "**" : 
+              lettersArray[i] = ' ';
+              break;
+            case "10" : 
+              lettersArray[i] += '.';
+              break;
+            case "11":
+              lettersArray[i] += '-';
+          } 
+        }
+        lettersArray[i] = MORSE_TABLE[lettersArray[i]] || " ";
+    }
+
+    return lettersArray.join("");
 }
 
 module.exports = {
